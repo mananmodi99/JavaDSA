8b2069ebfd03bcafda9e1763950f1ce475e6626c
mananmodi99 <mananmodi.0108@gmail.com>
2021-07-08 23:22:27 +0530

    Merge Sort Made easy

--- TimeandSpaceComplexity/MergeSort.java
+++ TimeandSpaceComplexity/MergeSort.java
@@ -4,53 +4,56 @@ public class MergeSort {
     public static void main(String[] args) {
        int[] arr = {20,10,30,50,60,5,80,25};
        int lo = 0, hi = arr.length-1;
-       int[] sorted = SortFunc(arr, lo, hi);
-        for (int i: sorted) {
+       SortFunc(arr, lo, hi);
+        for (int i: arr) {
             System.out.println(i);
         }
     }
 
-    private static int[] SortFunc(int[] arr, int lo, int hi) {
-        if(lo == hi){
-            int[] base = new int[1];
-            base[0] = arr[lo];
-            return base;
+    private static void SortFunc(int[] arr, int lo, int hi) {
+        if(lo<hi){
+            int mid = lo+(hi-lo)/2 ;//to avoid overflow
+            SortFunc(arr, lo, mid); //left
+            SortFunc(arr, mid+1, hi); //right
+            MergeTwoSortedArr(arr, lo, hi, mid);
         }
-        int mid = (lo+hi)/2;
-        int[] arr1 = SortFunc(arr, lo, mid);
-        int[] arr2 = SortFunc(arr, mid+1, hi);
-        return MergeTwoSortedArr(arr1, arr2);
     }
 
-    public static int[] MergeTwoSortedArr(int[] arr1, int[] arr2){
-        int[] merged = new int[arr1.length+arr2.length];
-        int i=0,j=0,k=0;
+    public static void MergeTwoSortedArr(int[] arr, int lo, int hi, int mid){
+        int arr1[] = new int[mid-lo+1];
+        int arr2[] = new int[hi - mid];
+        for(int i = 0; i<arr1.length; i++){
+            arr1[i] = arr[i+lo];
+        }
+        for(int i = 0 ; i<arr2.length ; i++){
+            arr2[i] = arr[mid+1+i];
+        }
+        int i=0,j=0,k=lo;
         while(i<arr1.length && j<arr2.length){
             if (arr1[i] <= arr2[j]) {
-                merged[k] = arr1[i];
+                arr[k] = arr1[i];
                 i++;
                 k++;
             }
             else{
-                merged[k] = arr2[j];
+                arr[k] = arr2[j];
                 j++;
                 k++;
             }
         }
         if(i== arr1.length){
             while(j<arr2.length){
-                merged[k] = arr2[j];
+                arr[k] = arr2[j];
                 j++;
                 k++;
             }
         }
         else{
             while(i<arr1.length){
-                merged[k] = arr1[i];
+                arr[k] = arr1[i];
                 i++;
                 k++;
             }
         }
-        return merged;
     }
 }
